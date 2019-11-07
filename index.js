babff7fa8ec4f58dbe6dd4823a0a71e43b3c5adb
Bry Cox <carl.cc@gmail.com>
2019-11-07 09:50:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ function submitData(name, email) {
     email: email
   };
 
-
+  const configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
 
 return fetch("http://localhost:3000/users", configObj)
   .then((response) => { return response.json(); })
